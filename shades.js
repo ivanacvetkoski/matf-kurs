dc0449a3b188abef7d1af87753657626e6858b17
ivanacvetkoski <ivana.cvetkoski@gmail.com>
2020-02-25 16:19:23 +0100

    Sitne izmene

--- shades.js
+++ shades.js
@@ -1,23 +1,23 @@
-var numberOfBoxes = 10;
+var clicks = 0;
+var level = 1;
 var shades = [];
 var oneShade;
-var body = document.querySelector("body");
 var boxes = document.querySelectorAll(".box");
 var mainBox = document.querySelector(".mainBox");
 var message = document.querySelector("#message");
-var h1 = document.querySelector("h1");
 
 main();
 
 function main(){
+	message.textContent="Ready?";
 	set();
-	alignBoxes();
+	checkBoxes();
 }
 
 function set(){
 
 	// pravimo niz random boja pozivom funkcije randomShades
-	shades = randomShades(numberOfBoxes);
+	shades = randomShades();
 	
 	// biramo jednu od njih da bude trazena 
 	var random = Math.floor(Math.random() * shades.length);
@@ -30,68 +30,119 @@ function set(){
 			boxes[i].style.background = shades[i];
 		}
 	}
-	h1.style.background = "white";
 }
 
-function randomShades(num){
+function randomShades(){
 
-	// slucajno biramo jedn boju, a ostale dobijamo menjanjem nje
+	// slucajno biramo jednu boju, a ostale dobijamo menjanjem nje
 	var array = []
 	
-	var r = Math.floor(Math.random() * 256);
-	var g = Math.floor(Math.random() * 256);
-	var b = Math.floor(Math.random() * 256);
+	var red = Math.floor(Math.random() * 256);
+	var green = Math.floor(Math.random() * 256);
+	var blue = Math.floor(Math.random() * 256);
 
-	array.push("rgb(" + r + ", " + g + ", " + b + ")");
+	array.push("rgb(" + red + ", " + green + ", " + blue + ")");
 
-	var nR=r;
-	var nG=g;
-	var nB=b;
+	var newRed=red;
+	var newGreen=green;
+	var newBlue=blue;
 
-	for(var i = 1; i < num; i++){
-		if(i<num/3){
-			if(nR<100)
-				nR=nR+30;
+	for(var i = 1; i < 10; i++){
+		if(i<10/3){
+			if(newRed<100)
+				newRed=newRed+30;
 			else
-				nR=nR-40;
-			array.push("rgb(" + nR + ", " + g + ", " + b + ")");
-		}else if(i<num/3*2){
-			if(nG<100)
-				nG=nG+30;
+				newRed=newRed-40;
+			array.push("rgb(" + newRed + ", " + green + ", " + blue + ")");
+		}else if(i<10/3*2){
+			if(newGreen<100)
+				newGreen=newGreen+30;
 			else
-				nG=nG-40;
-			array.push("rgb(" + r + ", " + nG + ", " + b + ")");
+				newGreen=newGreen-40;
+			array.push("rgb(" + red + ", " + newGreen + ", " + blue + ")");
 		}else{
-			if(nB<100)
-				nB=nB+30;
+			if(newBlue<100)
+				newBlue=newBlue+30;
 			else
-				nB=nB-40;
-			array.push("rgb(" + r + ", " + g + ", " + nB + ")");
+				newBlue=newBlue-40;
+			array.push("rgb(" + red + ", " + green + ", " + newBlue + ")");
 		}
 	}
 	return array;
 }
 
-function alignBoxes(){
+function nextLevel(){
+	set();
+	checkBoxes();
+}
+
+function checkBoxes(){
 
 	// proveravamo da li je korisnik pogodio boju
 	for(var i = 0; i < boxes.length; i++){
-		boxes[i].addEventListener("click", function(){
-
-			if(this.style.background === oneShade){
-				message.textContent = "YES";
-				h1.style.background = "pink";
-				body.style.background = "pink";
-
-				for(var i = 0; i < boxes.length; i++){
-					if(boxes[i].style.background !== oneShade)
-						boxes[i].style.background="pink";
-					}
-					
-			} else {
-					this.style.background = "#232323";
-					message.textContent = "No...Try again"
-			}
-		});
+		boxes[i].addEventListener("click", check);
 	}
 }
+
+function check(){
+	
+	if(this.style.background === oneShade){
+		
+		if(level < 6){
+			level = level + 1;
+			nextLevel();
+		}else{
+			var username = loadUsername();
+            sendResult(username, 50-clicks);
+            getResult(); 
+			clicks=0;
+			level = 1;
+			message.textContent = "Ready?";
+			set();
+			return;
+		}
+
+	} else {
+			clicks=clicks+1;
+			this.style.background = "#232323";
+			
+	}
+	message.textContent = "Score:" + (50-clicks);
+}
+
+const sendResult = async (username, result) => {
+    try { 
+        const URL = 'http://localhost:3002/';
+        const response = await fetch(URL, {
+            method : 'POST',
+            headers : {
+                'Content-Type': 'application/json'
+            },
+            mode : 'cors',
+            body : JSON.stringify({
+                name : username,
+                score : result
+            })
+        });
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+function loadUsername() {
+	//kraj igre, korisnik upisuje ime
+    let name = prompt("Enter your name", "Name");
+    let username = "";
+
+    if (name == null || name == "") {
+        username = "User cancelled the prompt.";
+    } else {
+        username = name;
+    }
+    
+    return username;
+};
